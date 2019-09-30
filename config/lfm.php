188c95fb80c861c0bc9f67a727b5e7d2070db6e2
jivko-jelev <jivkojelev01@gmail.com>
2019-09-30 22:59:02 +0300

    laravel file manager integration

--- config/lfm.php
+++ config/lfm.php
@@ -24,7 +24,7 @@ return [
     */
 
     // If true, private folders will be created for each signed-in user.
-    'allow_multi_user' => true,
+    'allow_multi_user' => false,
     // If true, share folder will be created when allow_multi_user is true.
     'allow_share_folder' => true,
 
@@ -110,7 +110,7 @@ return [
 
     // permissions to be set on file upload.
     'create_file_mode' => 0644,
-    
+
     // If true, it will attempt to chmod the file after upload
     'should_change_file_mode' => true,
 
